a306e9f2cafdafae5641e786b1ae5125ebed0efc
anuragpeshne <anurag.peshne@gmail.com>
2015-06-30 23:11:17 +0530

    implemeted berkeley sockets

---
+++ pomoClient.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+
+#define SOCK_PATH "/tmp/pomoSock"
+
+int main() {
+  int rSocketFd, len, t;
+  struct sockaddr_un remote;
+  char statusMsg[100];
+
+  if ((rSocketFd = socket(AF_UNIX, SOCK_STREAM, 0)) == -1) {
+    perror("socket");
+    exit(1);
+  }
+  remote.sun_family = AF_UNIX;
+  strcpy(remote.sun_path, SOCK_PATH);
+  len = strlen(remote.sun_path) + sizeof(remote.sun_family);
+  if (connect(rSocketFd, (struct sockaddr *)&remote, len) == -1) {
+    perror("connect");
+    exit(1);
+  }
+
+  if ((t=recv(rSocketFd, statusMsg, 100, 0)) > 0) {
+    statusMsg[t] = '\0';
+    printf("echo> %s", statusMsg);
+  } else {
+    if (t < 0) perror("recv");
+    else printf("Server closed connection\n");
+    exit(1);
+  }
+
+  return 0;
+}
--- pomoDaemon.c
+++ pomoDaemon.c
@@ -1,25 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <errno.h>
+#include <string.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <sys/socket.h>
+#include <sys/un.h>
 #include <signal.h>
 #include <math.h>
+#include <fcntl.h>
 
-void startTimer(int);
+#define SOCK_PATH "/tmp/pomoSock"
+#define TIMER (1 * 60)
+#define TIMER_SPEED 1
+#define PBAR_RESOLUTION 20
+#define PSTR_SIZE (1 + 1 + 2 + PBAR_RESOLUTION + 1)
+
+void tick(int);
 void resetTimer();
-void printElapsedTime(int);
-void printProgressBar(int, int, int);
-
-const int timer = 1 * 60;
-const int timerSpeed = 1;
-const int progressBarResolution = 20;
+void getPBarStr(char*);
 
 volatile int elapsedTime = 0;
 
 int main() {
   FILE *fp= NULL;
   struct sigaction sa, resetSa;
+  int lSocketFd, rSocketFd, len;
+  socklen_t t;
+  struct sockaddr_un local, remote;
+  char progressStr[PSTR_SIZE];
 
   pid_t process_id = 0;
   pid_t sid = 0;
@@ -55,69 +65,78 @@ int main() {
   chdir("/");
   // Close stdin. stdout and stderr
   close(STDIN_FILENO);
-  close(STDERR_FILENO);
-
-  sa.sa_handler = printElapsedTime;
-  sa.sa_flags = 0;
-  sigemptyset(&sa.sa_mask);
-  //attach handlers
-  if (sigaction(SIGUSR1, &sa, NULL) == -1) {
-    perror("sigaction");
-    exit(1);
-  }
+  //close(STDERR_FILENO);
 
   resetSa.sa_handler = resetTimer;
   resetSa.sa_flags = 0;
   sigemptyset(&resetSa.sa_mask);
-  if (sigaction(SIGUSR2, &resetSa, NULL) == -1) {
+  if (sigaction(SIGUSR1, &resetSa, NULL) == -1) {
     perror("sigaction");
     exit(1);
   }
-  
-  liveForever();
+
+  if ((lSocketFd = socket(AF_UNIX, SOCK_STREAM, 0)) == -1) {
+    perror("socket");
+    exit(1);
+  }
+  //fcntl(lSocketFd, F_SETFL, O_NONBLOCK);
+  local.sun_family = AF_UNIX;
+  strcpy(local.sun_path, SOCK_PATH);
+  unlink(local.sun_path);
+  len = strlen(local.sun_path) + sizeof(local.sun_family);
+  if (bind(lSocketFd, (struct sockaddr *)&local, len) == -1) {
+    perror("listen");
+    exit(1);
+  }
+  t = sizeof(remote);
+
+  while(1) {
+    sleep(1 / TIMER_SPEED);
+    tick(TIMER);
+
+    if ((rSocketFd = accept(lSocketFd, (struct sockaddr *)&remote, &t)) != -1) {
+      getPBarStr(progressStr);    //we are passing by referrence
+      if (send(rSocketFd, progressStr, PSTR_SIZE, 0) < 0){
+        perror("send");
+      }
+
+      close(rSocketFd);
+    }
+  }
   // you shall not pass - through this code path ever!
+  printf("exiting");
   return 0;
 }
 
-void liveForever() {
-  while(1) {
-    sleep(1);
-    startTimer(timer);
-  }
-}
-
-void startTimer(int totalTime) {
-  while(elapsedTime < totalTime) {
-    sleep(1 / timerSpeed);
+void tick(int totalTime) {
+  printf("tick");
+  if (elapsedTime < totalTime) {
     elapsedTime++;
   }
 }
 
 void resetTimer() {
   elapsedTime = 0;
-  liveForever();
 }
 
-void printElapsedTime(int sig) {
-  printf("%d => ", elapsedTime);
-  printProgressBar(elapsedTime, timer, progressBarResolution);
-  printf("\n");
-  fflush(stdout);
-}
-
-void printProgressBar(int current, int total, int resolution) {
+void getPBarStr(char* statusStr) {
   int coloredPieces, blankPieces;
-  int i;
+  int i, strIndex;
 
-  coloredPieces = (int)floor((resolution * current) / total);
-  blankPieces = resolution - coloredPieces;
+  strIndex = 0;
+  statusStr[strIndex++] = (char) elapsedTime;
+  statusStr[strIndex++] = ' ';
 
-  printf("[");
-  for (i = 0; i < resolution; i++) {
+  coloredPieces = (int)floor((PBAR_RESOLUTION * elapsedTime) / TIMER);
+  blankPieces = PBAR_RESOLUTION - coloredPieces;
+
+  statusStr[strIndex++] = '[';
+  for (i = 0; i < PBAR_RESOLUTION; i++) {
     if (i < coloredPieces)
-      printf("=");
+      statusStr[strIndex++] = '=';
     else
-      printf("_");
+      statusStr[strIndex++] = ' ';
   }
-  printf("]");
+  statusStr[strIndex++] = ']';
+  statusStr[strIndex] = '\0';
 }
